85e2c56bfac500a8a61b56f05d8eab190881fe49
sonya <jijumbeck@gmail.com>
2024-07-02 22:14:48 +0300

    fix view

--- src/shared/ui/Rating.tsx
+++ src/shared/ui/Rating.tsx
@@ -19,7 +19,6 @@ export function Rating({
         <div
             style={{
                 display: 'flex',
-                zIndex: 1
             }}
             onMouseOut={() => setStarIndex(undefined)}
         >
